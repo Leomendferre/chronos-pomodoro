b83e4bfe56e7c95d839d9195bd7968b1ab268976
Leomendferre <leo.mend@hotmail.com>
2025-06-18 07:26:07 -0300

    Bugfix: DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
-import { DefaultInput } from './components/Defaultinput/idenx';
+import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
 import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Footer } from './components/Footer';
 
 export function App() {
   return (
